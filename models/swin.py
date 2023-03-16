b0b9fef2605530826b92118088e19d08a41de1c9
ethanbar11 <ethanbarbar@gmail.com>
2023-03-16 15:56:53 +0200

    Swin with ema vs Swin with mega was added

--- models/swin.py
+++ models/swin.py
@@ -14,7 +14,10 @@ import torch
 from einops.layers.torch import Rearrange
 from .SPT import ShiftedPatchTokenization
 from einops import rearrange
+
+from .mega.exponential_moving_average import MultiHeadEMA
 from .mega.moving_average_gated_attention import MovingAverageGatedAttention
+from .mega.two_d_ssm_recursive import TwoDimensionalSSM
 
 
 class Mlp(nn.Module):
@@ -170,6 +173,121 @@ class WindowAttention(nn.Module):
         return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
 
 
+class WindowAttentionEMA(nn.Module):
+    r""" Window based multi-head self attention (W-MSA) module with relative position bias.
+    It supports both of shifted and non-shifted window.
+    Args:
+        dim (int): Number of input channels.
+        window_size (tuple[int]): The height and width of the window.
+        num_heads (int): Number of attention heads.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
+    """
+
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.,
+                 args=None, is_LSA=False):
+
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size  # Wh, Ww
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.is_LSA = is_LSA
+        if is_LSA:
+            self.scale = nn.Parameter(self.scale * torch.ones(self.num_heads))
+            self.mask = torch.eye((window_size[0] ** 2), (window_size[0] ** 2))
+            self.mask = torch.nonzero((self.mask == 1), as_tuple=False)
+            self.inf = float('-inf')
+
+        # define a parameter table of relative position bias
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+        # get pair-wise relative position index for each token inside the window
+        coords_h = torch.arange(self.window_size[0])
+        coords_w = torch.arange(self.window_size[1])
+        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+        relative_coords[:, :, 0] += self.window_size[0] - 1  # shift to start from 0
+        relative_coords[:, :, 1] += self.window_size[1] - 1
+        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+        self.register_buffer("relative_position_index", relative_position_index)
+
+        if args.ema == 'ema':
+            self.move = MultiHeadEMA(embed_dim=dim, ndim=args.ndim, bidirectional=True)
+        elif args.ema == 'ssm_2d':
+            self.move = TwoDimensionalSSM(embed_dim=dim, ndim=args.ndim, L=window_size[0] ** 2, args=args)
+
+        self.qk_proj = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.v_proj = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        trunc_normal_(self.relative_position_bias_table, std=.02)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x, mask=None):
+        """
+        Args:
+            x: input features with shape of (num_windows*B, N, C)
+            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
+        """
+        B_, N, C = x.shape
+        x = rearrange(x, 'b l h -> l b h')
+        x_ema = self.move(x)
+        x = rearrange(x, 'l b h -> b l h')
+
+        qk = self.qk_proj(x_ema).reshape(B_, N, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        v = self.v_proj(x).reshape(B_, N, 1, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)[0]
+        q, k = qk[0], qk[1]  # make torchscript happy (cannot use tensor as tuple)
+
+        if not self.is_LSA:
+            q = q * self.scale
+
+        else:
+            scale = self.scale
+            q = torch.mul(q, scale.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).expand((B_, self.num_heads, 1, 1)))
+
+        attn = (q @ k.transpose(-2, -1))
+
+        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
+        attn = attn + relative_position_bias.unsqueeze(0)
+
+        if mask is not None:
+            nW = mask.shape[0]
+            attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
+            attn = attn.view(-1, self.num_heads, N, N)
+
+            if self.is_LSA:
+                attn[:, :, self.mask[:, 0], self.mask[:, 1]] = self.inf
+            attn = self.softmax(attn)
+
+        else:
+            if self.is_LSA:
+                attn[:, :, self.mask[:, 0], self.mask[:, 1]] = self.inf
+            attn = self.softmax(attn)
+
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+    def extra_repr(self) -> str:
+        return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
+
+
 class SwinTransformerBlock(nn.Module):
     r""" Swin Transformer Block.
     Args:
@@ -207,14 +325,9 @@ class SwinTransformerBlock(nn.Module):
 
         self.norm1 = norm_layer(dim)
         if self.shift_size == 0:
-            if issubclass(norm_layer, nn.LayerNorm):
-                norm_type = 'layernorm'
-            print(norm_type)
-            self.attn = MovingAverageGatedAttention(embed_dim=dim, zdim=dim // 4, hdim=dim * 2, ndim=args.ndim,
-                                                    attention_activation='softmax',
-                                                    patch_amount=window_size ** 2, dropout=drop,
-                                                    attention_dropout=attn_drop, hidden_dropout=attn_drop,
-                                                    drop_path=drop_path,norm_type=norm_type, args=args)
+            self.attn = WindowAttentionEMA(
+                dim, window_size=to_2tuple(self.window_size), num_heads=num_heads,
+                qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, is_LSA=is_LSA, args=args)
         else:
             self.attn = WindowAttention(
                 dim, window_size=to_2tuple(self.window_size), num_heads=num_heads,
@@ -273,12 +386,8 @@ class SwinTransformerBlock(nn.Module):
         x_windows = x_windows.view(-1, self.window_size * self.window_size, C)  # nW*B, window_size*window_size, C
 
         # W-MSA/SW-MSA
-        if self.shift_size == 0:
-            x_windows = rearrange(x_windows, 'b l h -> l b h')
-            attn_windows = self.attn(x_windows, mask=self.attn_mask)  # nW*B, window_size*window_size, C
-            attn_windows = rearrange(attn_windows, 'l b h -> b l h')
-        else:
-            attn_windows = self.attn(x_windows, mask=self.attn_mask)  # nW*B, window_size*window_size, C
+        attn_windows = self.attn(x_windows, mask=self.attn_mask)  # nW*B, window_size*window_size, C
+
         # merge windows
         attn_windows = attn_windows.view(-1, self.window_size, self.window_size, C)
         shifted_x = window_reverse(attn_windows, self.window_size, H, H)  # B H' W' C
