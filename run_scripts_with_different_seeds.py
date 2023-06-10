6185e62a71cebca9e22ee8e1109483ff5a8db8d3
ethanbar11 <ethanbarbar@gmail.com>
2023-06-10 14:42:23 +0300

    ff

--- run_scripts_with_different_seeds.py
+++ run_scripts_with_different_seeds.py
@@ -6,7 +6,7 @@ command = "python main.py --model {model} --dataset {dataset} " \
           "--project {dataset}_Cait_Comparison --name {name} --seed {seed} --ema {ema} " \
           "--n_ssm {n_ssm} --ndim {ndim} --sd {sd} --gpu {gpu}"
 # models = {'vit': ['ssm_2d', 's4nd']}
-params = {'sd': [0.1, 0.15, 0.2, 0.3], 'n_ssm': [2, 8], 'dataset': ['CIFAR100'], 'model': ['convit'],
+params = {'sd': [0.1, 0.15, 0.2, 0.3], 'n_ssm': [2, 8], 'dataset': [''], 'model': ['convit'],
           'ema': ['ssm_2d'], 'seed': [0], 'ndim': [16]}
 
 seed_amount = 1
